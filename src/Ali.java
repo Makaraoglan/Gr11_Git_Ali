cd2535b3600cb9274138fd642d36a6daf33fa3ce
Ali Karaoglan <karaoglan.a.m@gmail.com>
2022-12-15 22:03:04 +0300

    last change

--- src/Ali.java
+++ src/Ali.java
@@ -2,5 +2,6 @@ public class Ali {
     public static void main(String[] args) {
         System.out.println("first change");
         System.out.println("second change");
+        System.out.println("last change");
     }
 }
